e93352f462e7568324bb318f54280f3db9961024
Bipin Gupta <bipin_gupta@yahoo.com>
2020-07-17 22:30:43 +0530

    cleaning

--- vagrant-jenkins/scripts/install-plugins.sh
+++ vagrant-jenkins/scripts/install-plugins.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-echo "***************************** SNAPD - Installation *****************************"
-
 set -o pipefail
 
 JENKINS_WAR=${JENKINS_WAR:-/usr/share/jenkins/jenkins.war}
